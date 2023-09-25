8dc2fd13e28eb47f86a3932f541e8392c6541dc3
LoadingBar0 <onlyforspam1273@gmail.com>
2023-09-25 15:56:48 -0400

    Removed the unneed passing of varible and *cough* *cough* changed gerneate to generate

--- game.py
+++ game.py
@@ -16,8 +16,8 @@ class Game(tk.Frame):
     def game(self, difficulty):
         """This is a simple game to guess a letter from the alphabet"""
         letters = []
-        original_story = self.gerneate_story()
-        story = original_story
+        self.generate_story()
+        story = STORY
         if  difficulty <= 5:
             replacement = "_"
         elif difficulty <= 10:
@@ -33,21 +33,21 @@ class Game(tk.Frame):
             else:
                 i -= 1
 
-        story = self.replace_letters(original_story, letters, replacement)
+        story = self.replace_letters(letters, replacement)
 
-        self.create_game_widgets(story, original_story, letters, replacement)
+        self.create_game_widgets(story, letters, replacement)
         
-    def replace_letters(self, orignal_story, letters, replacement):
+    def replace_letters(self, letters, replacement):
         """
         This function replaces all letters in the story with the replacement character
         """
-        story = orignal_story
+        story = STORY
         for letter in letters:
             story = story.replace(letter, replacement)
             story = story.replace(letter.upper(), replacement)
 
         if not letters:
-            story = orignal_story
+            story = STORY
         
         return story
 
@@ -79,7 +79,7 @@ class Game(tk.Frame):
         self.submit = tk.Button(self, text="Submit", fg="green", command=lambda: self.check_answer(original_story, letters, replacement))
         self.submit.pack(side="top")
 
-    def check_answer(self, oringal_story, letters, replacement):
+    def check_answer(self, letters, replacement):
         global guesses #This is the number of guesses the player has left I need to find a way to do this without using a global variable but for now this will do
         answer = self.answer_box.get().lower()
         if len(answer) > 1:
@@ -88,7 +88,7 @@ class Game(tk.Frame):
             return
         if answer in letters:
             self.barrier.config(text="You guessed it right!", fg="green")
-            self.update_game(oringal_story, answer, letters, replacement)
+            self.update_game(answer, letters, replacement)
             try:
                 self.answer_box.delete(0, tk.END)
             except tk.TclError:
@@ -109,10 +109,10 @@ class Game(tk.Frame):
                             command=self.master.destroy)
             self.quit.pack(side="bottom")
 
-    def update_game(self, orignal_story, answer, letters, replacement):
+    def update_game(self, answer, letters, replacement):
         story = self.story_box.get("1.0", tk.END)
         letters.remove(answer)
-        story = self.replace_letters(orignal_story, letters, replacement)
+        story = self.replace_letters(STORY, letters, replacement)
         self.remaining.config(text="Remaining letters: " + str(len(letters)))
         self.story_box.config(state=tk.NORMAL)
         self.story_box.delete("1.0", tk.END)
@@ -133,12 +133,14 @@ class Game(tk.Frame):
         self.difficulty.destroy()
         self.quit.destroy()
 
-    def gerneate_story(self):
+    def generate_story(self):
+        global STORY
+        #I do really got to figure out how to use tkinter vars
         """
         This function generates a story using Chat GTP api.
         """
-        sample = "In the quaint village of Everwood, nestled beneath the towering mountains, lived a gifted inventor named Felix Whizbang. His workshop, a chaotic blend of gears and gizmos, was the heart of the town. One day, a mysterious portal materialized before him, revealing Zara, an adventurer with a glowing amulet.\n\nZara explained the amulet's purpose: to find the scattered ingredients for the legendary " + '"Elixir of Allure, "' + "a potion that bestowed irresistible charm. Together, they embarked on a whimsical quest through the Fantasylands, facing riddles and befriending fantastical creatures.\n\nTheir journey not only uncovered the elixir's secret but also revealed the true magic lay in the bonds of friendship forged along the way. In the end, they shared the elixir's charm with their village, making Everwood a place where even eccentric inventions found a warm welcome."
-        return sample # For now we will use a sample story
+        STORY = "In the quaint village of Everwood, nestled beneath the towering mountains, lived a gifted inventor named Felix Whizbang. His workshop, a chaotic blend of gears and gizmos, was the heart of the town. One day, a mysterious portal materialized before him, revealing Zara, an adventurer with a glowing amulet.\n\nZara explained the amulet's purpose: to find the scattered ingredients for the legendary " + '"Elixir of Allure, "' + "a potion that bestowed irresistible charm. Together, they embarked on a whimsical quest through the Fantasylands, facing riddles and befriending fantastical creatures.\n\nTheir journey not only uncovered the elixir's secret but also revealed the true magic lay in the bonds of friendship forged along the way. In the end, they shared the elixir's charm with their village, making Everwood a place where even eccentric inventions found a warm welcome."
+        return 
         
 
 def main():
