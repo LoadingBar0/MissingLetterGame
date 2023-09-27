af11ca7289cbef896c5b4f9e4f4498dce0e43c68
LoadingBar0 <onlyforspam1273@gmail.com>
2023-09-27 00:50:57 -0400

    Added resize function but it is still really rough

--- game.py
+++ game.py
@@ -8,13 +8,12 @@ class Game(tk.Frame):
         self.master = master
         self.pack(expand=True, fill=tk.BOTH)
         self.create_start_widgets()
-        self.height = self.getscreensize()[1]
-        self.width = self.getscreensize()[0]
+        self.height = (self.getscreensize()[1] // 2)
+        self.width = (self.getscreensize()[0] // 2)
         self.master.geometry(str(self.width) + "x" + str(self.height))
-        self.width = self.winfo_width()
-        self.height = self.winfo_height()
         self.master.title("Missing Letter Game")
-        # self.bind("<Return>", lambda x: self.check_answer()) # This will be aused as a short cut to submit the answer Currently not working
+        self.master.minsize(800, 600)
+        # self.bind("<Return>", lambda x: self.check_answer()) # This will be used as a short cut to submit the answer Currently not working
         self.bind("<Configure>", lambda x: self.resize())
         
     def getscreensize(self):
@@ -28,13 +27,58 @@ class Game(tk.Frame):
         """
         Gets new window size when window is resized
         """
-        # To be added later
+        self.scale = ((self.winfo_width() / self.width) * (self.winfo_height() / self.height)) ** 0.5
+        self.width = self.winfo_width()
+        self.height = self.winfo_height()
+        self.scale_objects()
 
-    def scale(self, object, x1, y1, x2, y2):
+    def scale_objects(self):
         """
-        Scales all ojects in the canvas
+        Scales all ojects in the canvas size
         """
-        # To be added later
+        try:
+            self.story_box.config(height=(self.height // 50), width=(self.width // 20), font=("Courier", int(self.story_box_font_size * self.scale)))
+            self.story_box_font_size = self.story_box_font_size * self.scale
+            self.story_box.update()
+        except (AttributeError, tk.TclError):
+            pass
+        try:
+            self.barrier.config(font=("Courier", int(self.barrier_font_size * self.scale)))
+            self.barrier_font_size = self.barrier_font_size * self.scale
+            self.barrier.update()
+        except (AttributeError, tk.TclError):
+            pass
+        try:
+            self.start_button.config(font=("Courier", int(self.start_button_size * self.scale)))
+            self.start_button_size = self.start_button_size * self.scale
+            self.start_button.update()
+        except (AttributeError, tk.TclError):
+            pass
+        try:
+            self.difficulty_scale.config(font=("Courier", int(self.difficulty_scale_size * self.scale)), length=(self.difficulty_scale_size * 4 * self.scale))
+            self.difficulty_scale_size = self.difficulty_scale_size * self.scale
+            self.difficulty_scale.update()
+        except (AttributeError, tk.TclError):
+            pass
+        try:
+            self.quit_button.config(font=("Courier", int(self.quit_button_size * self.scale)))
+            self.quit_button_size = self.quit_button_size * self.scale
+            self.quit_button.update()
+        except (AttributeError, tk.TclError):
+            pass
+        try:
+            self.answer_box.config(font=("Courier", int(self.awnser_box_size * self.scale)), width=round(self.awnser_box_size / 7 * self.scale))
+            self.awnser_box_size = self.awnser_box_size * self.scale
+            self.answer_box.update()
+        except (AttributeError, tk.TclError):
+            pass
+        try:
+            self.submit.config(font=("Courier", int(self.submit_size * self.scale)))
+            self.submit_size = self.submit_size * self.scale
+            self.submit.update()
+        except (AttributeError, tk.TclError):
+            pass
+        
 
     def game(self, difficulty):
         """This is a simple game to guess a letter from the alphabet"""
@@ -79,34 +123,44 @@ class Game(tk.Frame):
         self.story = story
 
     def create_start_widgets(self):
-        self.barrier = tk.Label(self, text="Welcome to the Missing Letter Game!")
+        width = self.getscreensize()[0]
+        height = self.getscreensize()[1]
+        self.barrier_font_size = (((width / 100) * (height / 100)) ** 0.5) * 2 # This is a formula to scale the font size based on the screen size
+        self.barrier = tk.Label(self, text="Welcome to the Missing Letter Game!", font=("Courier", int(self.barrier_font_size)))
         self.barrier.pack(side="top")
-        self.start_button = tk.Button(self)
+        self.start_button_size = (((width / 200) * (height / 200)) ** 0.5) * 2 # This is a formula to scale the font size based on the screen size
+        self.start_button = tk.Button(self, font=("Courier", int(self.start_button_size)))
         self.start_button["text"] = "Play Game\n(click me)"
-        self.start_button["command"] = lambda : self.game(self.difficulty.get()) #Grabs difficulty from the slider
+        self.start_button["command"] = lambda : self.game(self.difficulty_scale.get()) #Grabs difficulty from the slider
         self.start_button.pack(side="top")
-        self.difficulty = tk.Scale(self, from_=1, to=10, orient=tk.HORIZONTAL)
-        self.difficulty.pack(side="top")
+        self.difficulty_scale_size = (((width / 100) * (height / 100)) ** 0.5) * 2 # This is a formula to scale the font size based on the screen size
+        self.difficulty_scale = tk.Scale(self, from_=1, to=10, orient=tk.HORIZONTAL, font=("Courier", int(self.difficulty_scale_size)), length=(self.difficulty_scale_size * 4))
+        self.difficulty_scale.pack(side="top")
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
+        self.quit_button_size = (((width / 100) * (height / 100)) ** 0.5) # This is a formula to scale the font size based on the screen size
+        self.quit_button = tk.Button(self, text="QUIT", fg="red",font=("Courier", int(self.quit_button_size)),
                             command=self.master.destroy)
-        self.quit.pack(side="bottom")
+        self.quit_button.pack(side="top")
 
     def create_game_widgets(self, letters, replacement):
+        width = self.getscreensize()[0]
+        height = self.getscreensize()[1]
+        self.story_box_font_size = (((width / 100) * (height / 100)) ** 0.5) # This is a formula to scale the font size based on the screen size
         self.remove_start_widgets()
-        self.barrier = tk.Label(self, text="Guess a letter!")
-        self.remaining = tk.Label(self, text="Remaining letters: " + str(len(letters)))
-        self.remaining.pack(side="top")
-        self.story_box = tk.Text(self, height=12, width=95) #Sets the box to
+        self.barrier.config(text="Remaining Letters:" + str(len(letters)))
+        self.barrier.pack(side="top") 
+        self.story_box = tk.Text(self, height=((self.height // 50)), width=((self.width // 20)), font=("Courier", int(self.story_box_font_size)), wrap=tk.WORD)
         self.story_box.pack(side="top")
         self.scroll = tk.Scrollbar(self, command=self.story_box.yview)
         self.scroll.pack(side=tk.RIGHT, fill=tk.Y)
         self.story_box.config(yscrollcommand=self.scroll.set)
-        self.smooth_story_insert()
+        # self.smooth_story_insert()
         self.story_box.config(state=tk.DISABLED)
-        self.answer_box = tk.Entry(self, width=2)
+        self.awnser_box_size = (((width / 100) * (height / 100)) ** 0.5) # This is a formula to scale the font size based on the screen size
+        self.answer_box = tk.Entry(self, width=round(self.awnser_box_size / 7,), font=("Courier", int(self.awnser_box_size)))
         self.answer_box.pack(side="top")
-        self.submit = tk.Button(self, text="Submit", fg="green", command=lambda: self.check_answer(letters, replacement))
+        self.submit_size = (((width / 100) * (height / 100)) ** 0.5) # This is a formula to scale the font size based on the screen size
+        self.submit = tk.Button(self, text="Submit", fg="green", command=lambda: self.check_answer(letters, replacement), font=("Courier", int(self.submit_size)))
         self.submit.pack(side="top")
 
     def check_answer(self, letters, replacement):
@@ -123,7 +177,6 @@ class Game(tk.Frame):
             except tk.TclError:
                 pass
         else:
-            self.barrier.config(text="Try again!", fg="red")
             self.answer_box.delete(0, tk.END)
             self.update_guesses()
     
@@ -143,15 +196,14 @@ class Game(tk.Frame):
             self.barrier.config(text="You lost!")
             self.answer_box.destroy()
             self.submit.destroy()
-            self.quit = tk.Button(self, text="QUIT", fg="red",
+            self.quit_button = tk.Button(self, text="QUIT", fg="red",
                             command=self.master.destroy)
-            self.quit.pack(side="top")
+            self.quit_button.pack(side="top")
 
     def update_game(self, answer, letters, replacement):
         letters.remove(answer)
         self.replace_letters(letters, replacement)
         story = self.story
-        self.remaining.config(text="Remaining letters: " + str(len(letters)))
         self.story_box.config(state=tk.NORMAL)
         self.story_box.delete("1.0", tk.END)
         self.story_box.insert(tk.END, story)
@@ -161,16 +213,15 @@ class Game(tk.Frame):
             self.barrier.config(text="You won!")
             self.answer_box.destroy()
             self.submit.destroy()
-            self.quit = tk.Button(self, text="QUIT", fg="red",
+            self.quit_button = tk.Button(self, text="QUIT", fg="red",
                             command=self.master.destroy)
-            self.quit.pack(side="top")
+            self.quit_button.pack(side="top")
 
 
     def remove_start_widgets(self):
-        self.barrier.config(text="")
         self.start_button.destroy()
-        self.difficulty.destroy()
-        self.quit.destroy()
+        self.difficulty_scale.destroy()
+        self.quit_button.destroy()
 
     def pick_story(self):
         """
